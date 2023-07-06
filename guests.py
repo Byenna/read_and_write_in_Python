f7c7a4b3bd5a08ad72d3386fad68346622a35311
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-06 13:16:19 +0200

    append name Emily

--- guests.py
+++ guests.py
@@ -3,3 +3,14 @@ with open("guests.txt", "r") as file:
     for line in file:
         print(line.strip()) # strip() removes newline characters
         
+# Append "Emily" to the file
+
+new_guest = "Emily"
+
+with open("guests.txt", "a") as file:
+    file.write("\n" + new_guest) # add newline before appending
+
+with open("guests.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
